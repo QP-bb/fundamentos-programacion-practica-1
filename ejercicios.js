9fb46ce8a63c58d19c8b2f4b679f8a885b7e3ca2
Eri Lopez <erilopezmendoza@outlook.com>
2025-11-05 11:33:05 -0600

    Ejercicio 2.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -100,7 +100,7 @@ function evaluarNota(nota) {
  */
 function mayorDeTres(a, b, c) {
   // TODO: Encuentra y retorna el mayor de los tres nÃºmeros
-  return 0;
+  return Math.max(a, b, c);
 }
 
 /**
