2a32ddfc67defa2bf7c308227f211c6074c3b09a
Eri Lopez <erilopezmendoza@outlook.com>
2025-11-05 11:04:53 -0600

    Ejercicio 1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Eri";
+  const edad = 24;
+  const carrera = "TCU";
   
   return { nombre, edad, carrera };
 }
